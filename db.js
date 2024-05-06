1430b81a6f4ad6b2b71a4f858f350bfc9c44a29c
Muhammad <148522477+magasov@users.noreply.github.com>
2024-05-06 08:16:05 +0200

    Add files via upload

---
+++ db.js
@@ -0,0 +1,26 @@
+const product = [
+  {
+    id: 1,
+    title: "Перфоратор универсальный Wander X645-46 GF 1450W",
+    hit: false,
+    articul: 'XJ89YHGO',
+    images: "./images/color1.svg",
+    price: '12 789 ₽',
+  },
+  {
+    id: 1,
+    title: "Перфоратор универсальный Wander X645-46 GF 1450W",
+    hit: false,
+    articul: 'XJ89YHGO',
+    images: "./images/color1.svg",
+    price: '12 789 ₽',
+  },
+  {
+    id: 1,
+    title: "Перфоратор универсальный Wander X645-46 GF 1450W",
+    hit: false,
+    articul: 'XJ89YHGO',
+    images: "./images/color1.svg",
+    price: '12 789 ₽',
+  }
+]
---
+++ js/top-arrow.js
@@ -0,0 +1,31 @@
+/*TOP_BUTTON*/
+const btnUp = {
+    el: document.querySelector('.btn-up'),
+    show() {
+        // удалим у кнопки класс btn-up_hide
+        this.el.classList.remove('btn-up_hide');
+    },
+    hide() {
+        // добавим к кнопке класс btn-up_hide
+        this.el.classList.add('btn-up_hide');
+    },
+    addEventListener() {
+        // при прокрутке содержимого страницы
+        window.addEventListener('scroll', () => {
+        // определяем величину прокрутки
+        const scrollY = window.scrollY || document.documentElement.scrollTop;
+        // если страница прокручена больше чем на 400px, то делаем кнопку видимой, иначе скрываем
+        scrollY > 400 ? this.show() : this.hide();
+        });
+    }
+    }
+    
+    btnUp.addEventListener();
+
+/*ANCHOR*/
+$('a[href^="#"]').click(function(){
+    let anchor = $(this).attr('href');
+    $('html, body').animate({
+    scrollTop: $(anchor).offset().top
+    }, 1300);
+});
